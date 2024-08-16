6bb07a135f7269fc36d2059d3e2bb05052182cf6
Abdul-Rahman Ibrahim <89408640+Abdul-Rahman-Ibrahim@users.noreply.github.com>
2024-08-17 02:27:13 +0300

    Update shanks_bot.py

--- shanks_bot.py
+++ shanks_bot.py
@@ -1,63 +1,48 @@
 from math import floor
 
 def is_prime(num):
-    for n in range(2,int(num**0.5)+1):
+    if num < 2:
+        return False
+    for n in range(2, int(num**0.5) + 1):
         if num % n == 0:
             return False
     return True
 
 def shanks_bot(prime_number):
-    value = "0." #instantiating numerical value of (1/prime_number) up to a precision equal to the shanks of the prime number
-    remainder = 1 #remainder in long division
-    lst = [] #list of remainders during long divison.
-    while True:
-        count = 0 #counts the number of zeros to be appended to the value variable
-        #implementing long division
-        while remainder < prime_number:
-            remainder = remainder * 10 #'add' one zero to the remainder if the divisor is less than the dividend
+    value = "0."  # Instantiate the numerical value of (1 / prime_number)
+    remainder = 1  # Start with the remainder 1 (as in long division)
+    remainder_list = []  # To store remainders seen during division
+    result_digits = []  # To store digits of the result
 
-            #counts the number of non-significant zeros to be added to 'value' if after the previous
-            #operation the remainder is still less than the prime number
-            count += 1
-            if count > 1:
-                value += str(0) #adds insignificant zero to 'value'
+    # Perform long division
+    while remainder not in remainder_list:
+        remainder_list.append(remainder)
 
-        #Exit loop if a remainder from the long divion is repeated...
-        if remainder in lst:
-            break
-        
-        #...otherwise we add the remainder to 'lst' and continue with the long divison.
-        lst.append(remainder)
-        digit = floor(remainder/prime_number) #gets the most significant digit from the divison
-        value += str(digit) #inserts the digit(s) to 'value'
-        
-        #gets remainder after the division. Alternatively we can use, (remainder = remainder % prime_number) to get the remainder
-        remainder = remainder - (floor(remainder/prime_number)*prime_number)
+        # Perform the next division step by multiplying the remainder by 10
+        remainder *= 10
+        digit = floor(remainder / prime_number)  # Get the next digit in the division
+        result_digits.append(str(digit))  # Append the digit to the result list
+        remainder = remainder % prime_number  # Update the remainder
+
         if remainder == 0:
-            print("{0} has 0 shanks".format(prime_number))
-            break
+            print(f"Reciprocal of {prime_number} is finite: {value}{''.join(result_digits)}")
+            return
 
-    del lst #delets 'lst' from memory.
+    # Identify where the repeating part starts
+    start_index = remainder_list.index(remainder)
+    non_repeating_part = ''.join(result_digits[:start_index])
+    repeating_part = ''.join(result_digits[start_index:])
 
-    #counts the number of insignificant zeros
-    if value[2:].startswith(str(0)):
-        i = 0
-        while value[2:][i] == str(0):
-            i += 1
-    else:
-        i = 0
-        
-    if remainder != 0:  
-        print("Reciprocal of {0} repeats after {1} figures".format(prime_number, len(value[2:])-i)) #prints the shanks
-        prompt_message = "Enter 1 if you wish to see the reciprocal of {0} before it starts to repeat. ".format(prime_number)
-        
-        ask = input(prompt_message)
-        if int(ask) == 1:
-            print(value[:-i])
+    print(f"Reciprocal of {prime_number} repeats after {len(result_digits) - start_index} figures.")
+    
+    # Ask the user if they want to see the reciprocal
+    show_value = input(f"Enter 1 if you wish to see the reciprocal of {prime_number} before it starts to repeat: ")
+    if int(show_value) == 1:
+        print(f"Reciprocal: {value}{non_repeating_part}({repeating_part})")
 
-##Test
-num = int(input("Enter a prime number to find its shank "))
+## Test the code
+num = int(input("Enter a prime number to find its shank: "))
 if is_prime(num):
     shanks_bot(num)
 else:
-    print("{0} is not a prime number. Exiting program.".format(num))
+    print(f"{num} is not a prime number. Exiting program.")
